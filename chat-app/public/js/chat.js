3fb306d69b0ad661acc798ed63314c41f5c5bd59
Gevorg <gevorgkarapetyan34@gmail.com>
2023-06-05 16:28:06 +0400

    feat:add chat app

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,33 +1,63 @@
-const socket = io()
+const socket = io();
 
-socket.on('message',(message)=>{
-    console.log(message)
-})
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
-document.querySelector('#message-form').addEventListener('submit',(e) => {
-    e.preventDefault()
-    const message = e.target.elements.message.value
-
-    socket.emit('sendMessage',message,(error)=>{
-        document.querySelector('#messageInput').value = ''
-        document.querySelector('#messageInput').focus()
-        if(error){
-            return console.log(error)
-        }
-        console.log(`Message Deilvered!`)
-    })
-})
-document.querySelector('#send-location').addEventListener('click',(e) => {
-    if(!navigator.geolocation){
-        return alert('Geolocation is not supported by your browser.')
+socket.on("message", (message) => {
+  console.log(message.text);
+  const html = Mustache.render(
+    document.querySelector("#message-template").innerHTML,
+    {
+      username: message.username,
+      message: message.text,
+      createdAt: moment(message.createdAt).format("h:mm a"),
     }
-    navigator.geolocation.getCurrentPosition((position)=>{
-        // console.log(position)
-        socket.emit('sendLocation',{
-            latitude:position.coords.latitude,
-            longitude:position.coords.longitude
-        },()=>{
-            console.log("Location has been shared")
-        })
+  );
+  document.querySelector("#messages").insertAdjacentHTML("beforeend", html);
+});
+socket.on('roomData',({room,users}) =>{
+    const html = Mustache.render(document.querySelector('#userList').innerHTML,{
+        room,
+        users
     })
+    document.querySelector('.sidebar').innerHTML=html
 })
+document.querySelector("#message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message, (error) => {
+    document.querySelector("#messageInput").value = "";
+    document.querySelector("#messageInput").focus();
+    if (error) {
+      return console.log(error);
+    }
+    console.log(`Message Deilvered!`);
+  });
+});
+document.querySelector("#send-location").addEventListener("click", (e) => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser.");
+  }
+  navigator.geolocation.getCurrentPosition((position) => {
+    // console.log(position)
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        console.log("Location has been shared");
+      }
+    );
+  });
+});
+
+socket.emit("join", { username, room }, (error) => {
+  if (error) {
+    alert(error);
+    location.href = "/";
+  }
+});
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,46 +1,83 @@
-const express = require('express')
-const path = require('path')
-const http = require('http')
-const socketio = require('socket.io')
-const Filter = require('bad-words')
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+const Filter = require("bad-words");
+const { generateMessage } = require("./utils/messages");
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} = require("./utils/users");
 
-const app = express()   
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname,'../public')
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-let count = 0
+let count = 0;
 
 //server(emit) -> client(recieve) -> Aknowledgement -> -countUpdated
 //client(emit) -> server(recieve) -> Aknowledgement -> -increment
 
-io.on('connection',(socket) => {
-    //konkret mekin
-    socket.emit('message',"Welcome!")
-    //saghin baci et mekic
-    socket.broadcast.emit('message',"A new user has connected")
-    socket.on('sendMessage',(message,callback) => {
-        //saghin
-        const filter = new Filter()
-        if(filter.isProfane(message)){
-            return callback('Profanity is not allowed')
-        }
-        io.emit('message',message)
-        callback('Delivered')
-    })
-    socket.on('sendLocation',(coords,callback)=>{
-        io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
-        callback('')
-    })
-    socket.on('disconnect',()=>{
-        io.emit('message',"A user has disconnected")
-    })
-})
+io.on("connection", (socket) => {
+  socket.on("join", (options, callback) => {
+    const { error, user } = addUser({ id: socket.id, ...options });
+    if (error) {
+      return callback(error);
+    }
 
-server.listen(port,()=>{
-    console.log(`Server is up on ${port}!`)
-})
+    socket.join(user.room);
+    //specific mekin
+    socket.emit("message", generateMessage("Admin", "Welcome!"));
+    //saghin baci et mekic,to n saghin karanq avelacnenq u nshenq orinak vorteghi saghin
+    socket.broadcast
+      .to(user.room)
+      .emit("message", generateMessage(`${user.username} has connected`));
+      io.to(user.room).emit('roomData',{
+        room:user.room,
+        users:getUsersInRoom(user.room)
+      })
+    callback();
+  });
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    //saghin
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed");
+    }
+    io.to(user.room).emit("message", generateMessage(user.username, message));
+    callback("Delivered");
+  });
+  socket.on("sendLocation", (coords, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit(
+      "message",
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    );
+    callback("");
+  });
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        generateMessage(`${user.username} has disconnected`)
+      );
+      io.to(user.room).emit('roomData',{
+        room:user.room,
+        users:getUsersInRoom(user.room)
+      })
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}!`);
+});
